ff3a9e1c81709b387cab0d9181989924c5fc6d5b
GERARD Alexandre <56207146+agerard57@users.noreply.github.com>
2022-10-14 00:13:37 +0200

    Various changes
    Remove comments
    Better naming
    Remove Serial prints
    Loop buzzer
    Screen blink
    
    Overall refactoring

--- vault.ino
+++ vault.ino
@@ -1,8 +1,8 @@
-/* Memory Library https://docs.arduino.cc/learn/built-in-libraries/eeprom */
 #include <EEPROM.h>
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
 #include <Keypad.h>
+
 Adafruit_7segment matrix = Adafruit_7segment();
 
 char keys[4][4] = {
@@ -10,52 +10,58 @@ char keys[4][4] = {
     {'4', '5', '6', 'B'},
     {'7', '8', '9', 'C'},
     {'*', '0', '#', 'D'}};
-byte rowPins[4] = {13, 12, 11, 10}; // connect to the row pinouts of the keypad
-byte colPins[4] = {9, 8, 7, 6};     // connect to the column pinouts of the keypad
+byte rowPins[4] = {13, 12, 11, 10};
+byte colPins[4] = {9, 8, 7, 6};
 
-Keypad myKeypad = Keypad(makeKeymap(keys), rowPins, colPins, 4, 4);
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, 4, 4);
 
-int numberAddress = 0; // Address that will store the number
-int resetButton = 0;   // 0 == LOW
+int numberAddress = 0;
+int resetButton = 0;
 int code = 1234;
 int displayedNumber = 0;
 
 int buzzerPin = 3;
 int resetButtonPin = 4;
 
-/* https://www.youtube.com/watch?v=NA5MwhuHWLo :) */
 void input()
 {
   tone(buzzerPin, 1760);
-  delay(150);
- noTone(buzzerPin);
-}
-
-void correct()
-{
-  tone(buzzerPin, 2300);
-  delay(150);
-  noTone(buzzerPin);
-  tone(buzzerPin, 2300);
   delay(150);
   noTone(buzzerPin);
 }
 
 void error()
 {
-  tone(buzzerPin, 700);
-  delay(200);
-  noTone(buzzerPin);
-  delay(100);
+  for (int i = 0; i < 3; i++)
+  {
+    screenStatus("off");
+    tone(buzzerPin, 700);
+    delay(200);
+    noTone(buzzerPin);
+    screenStatus("on");
+    delay(100);
+  }
+  clearScreen();
+}
 
-  tone(buzzerPin, 700);
-  delay(200);
-  noTone(buzzerPin);
+void correct()
+{
+  for (int i = 0; i < 2; i++)
+  {
+    screenStatus("off");
+    tone(buzzerPin, 2300);
+    delay(150);
+    noTone(buzzerPin);
+    screenStatus("on");
+    delay(50);
+  }
+  clearScreen();
+}
 
-  delay(100);
-  tone(buzzerPin, 700);
-  delay(200);
-  noTone(buzzerPin);
+void clearScreen()
+{
+  displayedNumber = 0;
+  screenStatus("off");
 }
 
 void setup()
@@ -75,59 +81,54 @@ void setup()
   pinMode(resetButtonPin, INPUT_PULLUP);
 
   /* GET number from memory */
-  //EEPROM.get(numberAddress, code);
+  // EEPROM.get(numberAddress, code);
+}
+
+void screenStatus(String status)
+{
+  if (status == "off")
+  {
+    matrix.clear();
+    matrix.writeDisplay();
+  }
+  else if (status == "on")
+  {
+    matrix.print(displayedNumber);
+    matrix.writeDisplay();
+  }
 }
 
 void loop()
 {
-  char keyPressed = myKeypad.getKey();
+  char keyPressed = keypad.getKey();
   if (displayedNumber >= 1000)
   {
-
-
     if (displayedNumber == code)
-    {
       correct();
-    }
     else
-    {
       error();
-    }
-   displayedNumber = 0;
-        matrix.print("");
-    matrix.writeDisplay();
   }
   if (keyPressed >= '0' && keyPressed <= '9')
-    {
+  {
 
-      displayedNumber = displayedNumber * 10 + (keyPressed - '0');
-      Serial.println(displayedNumber);
-    matrix.print(displayedNumber);
-    matrix.writeDisplay();
-    Serial.println(keyPressed);
+    displayedNumber = displayedNumber * 10 + (keyPressed - '0');
+    screenStatus("on");
     input();
   }
 
   if (displayedNumber != 0 && keyPressed == 'C')
-  {
-       displayedNumber = 0;
-    matrix.print("");
-    matrix.writeDisplay();
     error();
-  }
 
   resetButton = digitalRead(resetButtonPin);
 
-// If resetButton is hold down for 3 seconds, reset the code
+  // If resetButton is hold down for 3 seconds, reset the code
   if (resetButton == 0)
   {
     delay(3000);
     resetButton = digitalRead(resetButtonPin);
     if (resetButton == 0)
     {
-      displayedNumber = 0;
-      matrix.print("");
-      matrix.writeDisplay();
+      clearScreen();
       code = 2345;
       EEPROM.put(numberAddress, code);
       Serial.println("Code reset to 1234");
